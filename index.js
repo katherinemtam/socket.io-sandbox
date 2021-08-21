71ceed5eaa562d0fedfff779ece1c8e9fcc23f39
Katherine Tam <katherinemtam@gmail.com>
2021-08-21 13:38:57 -0700

    integrate socket.io

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+//initialize new instance of socket.io with HTTP server
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+//listen for incoming sockets
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  //when user disconnects
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
