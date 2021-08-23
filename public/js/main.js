0d8f1b482031a53fa25b835cad58bf7f3878960b
Katherine Tam <katherinemtam@gmail.com>
2021-08-22 21:20:47 -0700

    emit events/messages back and forth

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,7 @@
 // we have access to this because of the script added in chat.html (line 58)
 const socket = io();
+
+// catches "message" from server.js
+socket.on('message', message => {
+  console.log(message)
+})
--- server.js
+++ server.js
@@ -13,7 +13,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // run when client connects
 io.on('connection', socket => {
-  console.log('New WS Connection...')
+  // we catch this on the client side via main.js
+  // socket.emit will emit to the single user/client that is connecting
+  socket.emit('message', 'Welcome to ChatCord!');
+
+  // broadcast when a user connects
+  // socket.broadcast.emit will emit to everyone except the user that is connecting
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  //Runs when client disconnects
+  socket.on('disconnect', () => {
+    // io.emit will emit to everyone
+    io.emit('message', 'A user has left the chat');
+  });
 })
 
 const PORT = 3000 || process.env.PORT;
