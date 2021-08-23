36b2d9d4fb1ffdcac8faf17c914e2eea5f448d70
Katherine Tam <katherinemtam@gmail.com>
2021-08-22 22:40:17 -0700

    display username when disconnect

--- utils/users.js
+++ utils/users.js
@@ -3,9 +3,7 @@ const users = [];
 // join user to chat
 function userJoin(id, username, room) {
   const user = { id, username, room };
-
   users.push(user);
-
   return user;
 }
 
@@ -14,7 +12,25 @@ function getCurrentUser(id) {
   return users.find(user => user.id === id);
 }
 
+// user leaves chat
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+
+  // findIndex return numbers, if it cannot find index, it will return -1
+  if(index !== -1) {
+    // splice(start, deleteCount)
+    return users.splice(index, 1)[0];
+  }
+}
+
+// get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+}
+
 module.exports = {
   userJoin,
-  getCurrentUser
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
 }
